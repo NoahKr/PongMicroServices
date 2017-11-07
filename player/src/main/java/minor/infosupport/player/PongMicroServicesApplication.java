e1198dddbc8cecfbbb75db3cfaab439c47560181
Bob Soppe <bobsoppe@users.noreply.github.com>
2017-11-07 10:31:13 +0100

    Add project structure

---
+++ player/src/main/java/minor/infosupport/player/PongMicroServicesApplication.java
@@ -0,0 +1,12 @@
+package minor.infosupport.player;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PongMicroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PongMicroServicesApplication.class, args);
+	}
+}
