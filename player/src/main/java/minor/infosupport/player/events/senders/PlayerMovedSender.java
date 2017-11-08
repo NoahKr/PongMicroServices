48b7d1ea67bb58fe072c23bf63415d173f34d461
Bob Soppe <bobsoppe@users.noreply.github.com>
2017-11-08 13:47:44 +0100

    Refactor Player service

--- player/src/main/java/minor/infosupport/player/events/senders/PlayerMovedSender.java
+++ player/src/main/java/minor/infosupport/player/events/senders/PlayerMovedSender.java
@@ -1,20 +1,23 @@
 package minor.infosupport.player.events.senders;
 
-import org.springframework.amqp.core.Queue;
-import org.springframework.amqp.core.TopicExchange;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+@Component
 public class PlayerMovedSender {
+
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
 
 	@Autowired
 	private RabbitTemplate template;
 
-	@Autowired
-	private TopicExchange exchange;
-
 	public void send() {
-		template.convertAndSend(exchange.getName(), "player.moved", "TEST");
+		String message = "";
+		template.convertAndSend("pong", "player.moved", message);
+		logger.debug(message);
 	}
 
 }
