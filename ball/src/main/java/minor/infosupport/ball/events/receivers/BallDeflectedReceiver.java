eafc4fb3e8b1e93309d183a0bf319ba9d3187233
NoahKr <noahkroon1997@gmail.com>
2017-11-08 13:34:28 +0100

    changes???\!\!\!???

--- ball/src/main/java/minor/infosupport/ball/events/receivers/BallDeflectedReceiver.java
+++ ball/src/main/java/minor/infosupport/ball/events/receivers/BallDeflectedReceiver.java
@@ -4,12 +4,13 @@ import minor.infosupport.ball.services.BallService;
 import minor.infosupport.ball.controllers.BallController;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.amqp.rabbit.annotation.RabbitHandler;
-import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.amqp.core.ExchangeTypes;
+import org.springframework.amqp.rabbit.annotation.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.handler.annotation.Payload;
+import org.springframework.stereotype.Component;
 
-@RabbitListener(queues = "#{ballDeflectedQueue.name}")
+@Component
 public class BallDeflectedReceiver {
 
     private final Logger logger = LoggerFactory.getLogger(this.getClass());
@@ -17,11 +18,15 @@ public class BallDeflectedReceiver {
     @Autowired
     BallService ballService;
 
-    //This method should be called when a message arrives on the queue
-    @RabbitHandler
+    @RabbitListener(bindings = @QueueBinding(
+            value = @Queue,
+            exchange = @Exchange(
+                    value = "pong",
+                    type = ExchangeTypes.TOPIC,
+                    durable = "true"),
+            key = "ball.deflected"))
     public void receive(String message) throws InterruptedException {
         logger.debug(message);
-
         ballService.changeBallDirection(message);
     }
 }
