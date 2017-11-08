aa4160c2bf35a64b0eee53cbc4159d21f0813f46
NoahKr <noahkroon1997@gmail.com>
2017-11-08 13:49:23 +0100

    working on replayGame

--- game/src/main/java/minor/infosupport/game/events/listeners/CatchAllListener.java
+++ game/src/main/java/minor/infosupport/game/events/listeners/CatchAllListener.java
@@ -1,5 +1,7 @@
 package minor.infosupport.game.events.listeners;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.amqp.core.ExchangeTypes;
 import org.springframework.amqp.rabbit.annotation.Exchange;
 import org.springframework.amqp.rabbit.annotation.Queue;
@@ -12,6 +14,8 @@ import org.springframework.stereotype.Component;
 @Component
 public class CatchAllListener {
 
+	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+
 	@RabbitListener(bindings = @QueueBinding(
 			value = @Queue,
 			exchange = @Exchange(
@@ -20,7 +24,7 @@ public class CatchAllListener {
 					durable = "true"),
 			key = "#"))
 	public void listen(@Header(AmqpHeaders.RECEIVED_ROUTING_KEY) String key, String message) {
-		
+		logger.debug("Key: " + key);
+		logger.debug("Message" + message);
 	}
-
 }
