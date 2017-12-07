721ab2c1221d565e6db4845edd5360e3b3faaac9
Jimmy Nguyen <s1087315@student.hsleiden.nl>
2017-12-07 11:37:06 +0100

    fix alles

--- ball/src/main/java/minor/infosupport/ball/BallApplication.java
+++ ball/src/main/java/minor/infosupport/ball/BallApplication.java
@@ -2,8 +2,10 @@ package minor.infosupport.ball;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.bus.jackson.RemoteApplicationEventScan;
 
 @SpringBootApplication
+@RemoteApplicationEventScan
 public class BallApplication {
 
 	public static void main(String[] args) {
